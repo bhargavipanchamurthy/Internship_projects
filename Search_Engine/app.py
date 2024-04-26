5e15043a768ee2484140059162fdf2a424f4313e
bhargavipanchamurthy <147170615+bhargavipanchamurthy@users.noreply.github.com>
2024-04-26 13:18:51 +0530

    Add files via upload

---
+++ Search_Engine/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import joblib
+
+# Function to load data
+def load_data(csv_file):
+    data = pd.read_csv(csv_file)
+    return data
+
+# Function to load trained models
+def load_models():
+    count_vectorizer = joblib.load(r"C:\Users\Bhargavi\Downloads\count_vectorizer.pkl")
+    tfidf_transformer = joblib.load(r"C:\Users\Bhargavi\Downloads\tfidf_transformer.pkl")
+    tfidf_matrix = joblib.load(r"C:\Users\Bhargavi\Downloads\tfidf_matrix.pkl")
+    return count_vectorizer, tfidf_transformer, tfidf_matrix
+
+# Function to preprocess query
+def preprocess_query(query, count_vectorizer):
+    query_vector = count_vectorizer.transform([query])
+    return query_vector
+
+# Function to retrieve similar documents
+def retrieve_similar_documents(query, count_vectorizer, tfidf_transformer, tfidf_matrix, data):
+    query_vector = preprocess_query(query, count_vectorizer)
+    query_tfidf = tfidf_transformer.transform(query_vector)
+    similarity_scores = cosine_similarity(query_tfidf, tfidf_matrix)
+    sorted_indices = similarity_scores.argsort()[0][::-1]
+    top_indices = sorted_indices[:5]
+    return data.iloc[top_indices]["file_content_chunks"].tolist()
+
+# Main function
+def main():
+    st.title('Search Engine.....')
+
+    data = load_data(r"C:\Users\Bhargavi\Downloads\Sub_titles.csv")
+
+    count_vectorizer, tfidf_transformer, tfidf_matrix = load_models()
+
+    query = st.text_input('Enter movie name.....', '')
+
+    if st.button('Click here'):
+        if query:
+            retrieved_documents = retrieve_similar_documents(query, count_vectorizer, tfidf_transformer, tfidf_matrix, data)
+            st.subheader('Top 5 documents related to the query:')
+            for i, doc in enumerate(retrieved_documents, 1):
+                st.write(f"Document {i}: {doc}")
+
+if __name__ == '__main__':
+    main()
